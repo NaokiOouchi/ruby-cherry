1d947a15ff98ab4fa90c5505ad3c1c235f90d83d
OouchiNaoki <noouchi4@gmail.com>
2020-11-02 00:33:04 +0900

    No.3931 ブロック引数を一つにするとキーとバリューが配列に格納される

--- section5/hash_each.rb
+++ section5/hash_each.rb
@@ -6,3 +6,14 @@ end
 # japan: yen
 # us: doller
 # india: rupee
+
+# ブロック引数を一つにするとキーとバリューが配列に格納される
+currencies = { "japan" => "yen", "us" => "doller", "india" => "rupee" }
+currencies.each do |key_value|
+  key = key_value[0]
+  value = key_value[1]
+  puts "#{key}: #{value}"
+end
+# japan: yen
+# us: doller
+# india: rupee
