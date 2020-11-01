972156145be2d19238ead9cd612fd156bbb0e8bb
OouchiNaoki <noouchi4@gmail.com>
2020-11-02 00:29:33 +0900

    No.3908 hashはeachで繰り返し処理ができる

---
+++ section5/hash_each.rb
@@ -0,0 +1,8 @@
+currencies = { "japan" => "yen", "us" => "doller", "india" => "rupee" }
+
+currencies.each do |key, value|
+  puts "#{key}: #{value}"
+end
+# japan: yen
+# us: doller
+# india: rupee
